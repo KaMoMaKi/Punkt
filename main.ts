2308cb8fdf2d49dff1c06da0edc2a0e512bfa901
KaMoMaKi <lukaknott49@gmail.com>
2026-01-22 11:38:57 +0100

    Lampe Senden

--- main.ts
+++ main.ts
@@ -3,10 +3,44 @@ input.onGesture(Gesture.TiltRight, function () {
         XAktuell += 1
     }
 })
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    if (YAktuell < 1) {
+        if (XAktuell == 0) {
+            radio.sendValue("LED_AN", 0)
+        } else if (XAktuell == 1) {
+            radio.sendValue("LED_AN", 1)
+        } else if (XAktuell == 2) {
+            radio.sendValue("LED_AN", 2)
+        } else if (XAktuell == 3) {
+            radio.sendValue("LED_AN", 3)
+        } else {
+            basic.setLedColor(0xff0000)
+            basic.pause(500)
+            basic.turnRgbLedOff()
+        }
+    } else {
+        if (XAktuell == 0) {
+            radio.sendValue("LED_AUS", 0)
+        } else if (XAktuell == 1) {
+            radio.sendValue("LED_AUS", 1)
+        } else if (XAktuell == 2) {
+            radio.sendValue("LED_AUS", 2)
+        } else if (XAktuell == 3) {
+            radio.sendValue("LED_AUS", 3)
+        } else {
+            basic.setLedColor(0xff0000)
+            basic.pause(500)
+            basic.turnRgbLedOff()
+        }
+    }
+})
 input.onGesture(Gesture.TiltLeft, function () {
     if (!(XAktuell <= 0)) {
         XAktuell += -1
     }
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    basic.showNumber(pins.analogReadPin(AnalogPin.P0))
 })
 function Display () {
     if (!(XAlt == XAktuell && YAlt == YAktuell)) {
@@ -29,6 +63,33 @@ radio.onReceivedString(function (receivedString) {
         pins.digitalWritePin(DigitalPin.P0, 0)
     } else {
     	
+    }
+})
+input.onPinTouchEvent(TouchPin.P0, input.buttonEventDown(), function () {
+    basic.setLedColor(0xff0000)
+})
+radio.onReceivedValue(function (name, value) {
+    if (name == "LED_AN") {
+        if (value == 0) {
+            pins.digitalWritePin(DigitalPin.P0, 1)
+        } else if (value == 1) {
+            pins.digitalWritePin(DigitalPin.P1, 1)
+        } else if (value == 2) {
+            pins.digitalWritePin(DigitalPin.P2, 1)
+        } else if (value == 3) {
+            pins.digitalWritePin(DigitalPin.P3, 1)
+        }
+    }
+    if (name == "LED_AUS") {
+        if (value == 0) {
+            pins.digitalWritePin(DigitalPin.P0, 0)
+        } else if (value == 1) {
+            pins.digitalWritePin(DigitalPin.P1, 0)
+        } else if (value == 2) {
+            pins.digitalWritePin(DigitalPin.P2, 0)
+        } else if (value == 3) {
+            pins.digitalWritePin(DigitalPin.P3, 0)
+        }
     }
 })
 input.onGesture(Gesture.LogoUp, function () {
